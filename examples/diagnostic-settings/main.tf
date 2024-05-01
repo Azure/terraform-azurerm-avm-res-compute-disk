16dee8414350e99c166f1e4017aa12e787e214c4
terrymandin <terry.mandin@microsoft.com>
2024-05-01 09:43:11 -0600

    Diagnostic Settings Example

--- examples/diagnostic-settings/main.tf
+++ examples/diagnostic-settings/main.tf
@@ -1,8 +1,10 @@
-## Section to provide a random Azure region for the resource group
+# We need the tenant id for the key vault.
+data "azurerm_client_config" "this" {}
+
 # This allows us to randomize the region for the resource group.
 module "regions" {
   source  = "Azure/regions/azurerm"
-  version = "~> 0.3"
+  version = ">= 0.3.0"
 }
 
 # This allows us to randomize the region for the resource group.
@@ -10,12 +12,11 @@ resource "random_integer" "region_index" {
   max = length(module.regions.regions) - 1
   min = 0
 }
-## End of section to provide a random Azure region for the resource group
 
 # This ensures we have unique CAF compliant names for our resources.
 module "naming" {
   source  = "Azure/naming/azurerm"
-  version = "~> 0.3"
+  version = "0.3.0"
 }
 
 # This is required for resource modules
@@ -24,12 +25,13 @@ resource "azurerm_resource_group" "this" {
   name     = module.naming.resource_group.name_unique
 }
 
-data "azurerm_client_config" "current" {}
+resource "azurerm_log_analytics_workspace" "this" {
+  location            = azurerm_resource_group.this.location
+  name                = module.naming.log_analytics_workspace.name_unique
+  resource_group_name = azurerm_resource_group.this.name
+}
 
 # This is the module call
-# Do not specify location here due to the randomization above.
-# Leaving location as `null` will cause the module to use the resource group location
-# with a data source.
 module "disk" {
   source = "../../"
   # source             = "Azure/avm-res-compute-disk/azurerm"
@@ -42,13 +44,4 @@ module "disk" {
   create_option = "Empty"
   storage_account_type = "Premium_LRS"
   disk_size_gb = 1024
-
-  // Example role assignment
-  role_assignments = {
-    role_assignment = {
-      principal_id               = data.azurerm_client_config.current.object_id
-      role_definition_id_or_name = "Reader"
-      description                = "Assign the Reader role to the deployment user on this disk resource scope."
-    }
-  }
 }
