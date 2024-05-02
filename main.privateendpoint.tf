268a8e574cfbe9634ccc5eecb68852cf01aebc3f
terrymandin <terry.mandin@microsoft.com>
2024-05-02 14:28:52 -0600

    Private Endpoints Example

---
+++ main.privateendpoint.tf
@@ -0,0 +1,78 @@
+# The PE resource when we are managing the private_dns_zone_group block:
+resource "azurerm_private_endpoint" "this_managed_dns_zone_groups" {
+for_each                      = { for k, v in var.private_endpoints : k => v if var.private_endpoints_manage_dns_zone_group }
+name                          = each.value.name != null ? each.value.name : "pep-${var.name}"
+location                      = each.value.location != null ? each.value.location : var.location
+resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
+subnet_id                     = each.value.subnet_resource_id
+custom_network_interface_name = each.value.network_interface_name
+tags                          = each.value.tags
+
+private_service_connection {
+    name                           = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "pse-${var.name}"
+    private_connection_resource_id = var.disk_access_id
+    is_manual_connection           = false
+    subresource_names              = ["disks"] # map to each.value.subresource_name if there are multiple services.
+}
+
+dynamic "private_dns_zone_group" {
+    for_each = length(each.value.private_dns_zone_resource_ids) > 0 ? ["this"] : []
+
+    content {
+    name                 = each.value.private_dns_zone_group_name
+    private_dns_zone_ids = each.value.private_dns_zone_resource_ids
+    }
+}
+
+dynamic "ip_configuration" {
+    for_each = each.value.ip_configurations
+
+    content {
+    name               = ip_configuration.value.name
+    subresource_name   = "disks" # map to each.value.subresource_name if there are multiple services.
+    member_name        = "disks" # map to each.value.subresource_name if there are multiple services.
+    private_ip_address = ip_configuration.value.private_ip_address
+    }
+}
+}
+
+# The PE resource when we are managing **not** the private_dns_zone_group block:
+resource "azurerm_private_endpoint" "this_unmanaged_dns_zone_groups" {
+for_each = { for k, v in var.private_endpoints : k => v if !var.private_endpoints_manage_dns_zone_group }
+
+    name                          = each.value.name != null ? each.value.name : "pep-${var.name}"
+    location                      = each.value.location != null ? each.value.location : var.location
+    resource_group_name           = each.value.resource_group_name != null ? each.value.resource_group_name : var.resource_group_name
+    subnet_id                     = each.value.subnet_resource_id
+    custom_network_interface_name = each.value.network_interface_name
+    tags                          = each.value.tags
+
+    private_service_connection {
+        name                           = each.value.private_service_connection_name != null ? each.value.private_service_connection_name : "pse-${var.name}"
+        private_connection_resource_id = var.disk_access_id
+        is_manual_connection           = false
+        subresource_names              = ["disks"] # map to each.value.subresource_name if there are multiple services.
+    }
+
+    dynamic "ip_configuration" {
+        for_each = each.value.ip_configurations
+
+        content {
+        name               = ip_configuration.value.name
+        subresource_name   = "disks" # map to each.value.subresource_name if there are multiple services.
+        member_name        = "disks" # map to each.value.subresource_name if there are multiple services.
+        private_ip_address = ip_configuration.value.private_ip_address
+        }
+    }
+
+    lifecycle {
+        ignore_changes = [private_dns_zone_group]
+    }
+}
+
+resource "azurerm_private_endpoint_application_security_group_association" "this" {
+for_each                      = local.private_endpoint_application_security_group_associations
+private_endpoint_id           = azurerm_private_endpoint.this_managed_dns_zone_groups[each.value.pe_key].id
+application_security_group_id = each.value.asg_resource_id
+}
+
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
+# In your output you need to select the correct resource based on the value of var.private_endpoints_manage_dns_zone_group:
+output "private_endpoints" {
+value       = var.private_endpoints_manage_dns_zone_group ? azurerm_private_endpoint.this_managed_dns_zone_groups : azurerm_private_endpoint.this_unmanaged_dns_zone_groups
+description = <<DESCRIPTION
+A map of the private endpoints created.
+DESCRIPTION
+}
+
 # Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
 output "resource" {
